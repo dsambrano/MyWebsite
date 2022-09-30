a954b65e9d67a721c4817644611e28e365bf0ab3
Deshawn Sambrano <3dbrano3@gmail.com>
2022-09-30 13:56:39 -0700

    Separating out Index File: Just a Start

--- run_server.py
+++ run_server.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     # Subdomains in flask: https://www.geeksforgeeks.org/subdomain-in-flask-python/
     website_url = "flask-test.com:5000"
     app.config['SERVER_NAME'] = website_url
+    app.config['FLASK_ENV'] = "development"
+    app.config['FLASK_DEBUG'] = True
     app.run()
 
