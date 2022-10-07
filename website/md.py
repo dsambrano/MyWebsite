1312a34de3ffd5523fb56f161843cc3be1401186
Deshawn Sambrano <3dbrano3@gmail.com>
2022-10-06 22:28:53 -0700

    Adding Meta Extension to list of options

--- website/md.py
+++ website/md.py
@@ -4,7 +4,7 @@ import markdown
 
 def markdown_2_html(md_file: Path, *args, **kwargs):
     """Convert Markdown File to HTML"""
-    # extensions=["tables", "toc"]
+    # extensions=["tables", "toc", "meta"]
     with open(model, "r") as f: 
         test = f.read()
     html = markdown.markdown(test, *args, **kwargs)
